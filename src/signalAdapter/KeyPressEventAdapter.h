e311b63c5d322c0c12dc6392d41ddd064356e8b6
Iwasz <lukasz.iwaszkiewicz@gmail.com>
2017-02-20 21:13:04 +0100

    KeyPressEventAdapter wip

---
+++ src/signalAdapter/KeyPressEventAdapter.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
+#ifndef KEYPRESSEVENTADAPTER_H
+#define KEYPRESSEVENTADAPTER_H
+
+
+class KeyPressEventAdapter
+{
+public:
+        KeyPressEventAdapter();
+};
+
+#endif // KEYPRESSEVENTADAPTER_H
